1f36bdddee7ccdbfee32f4a77aea7dd05f7f2515
Paulina Pacura <riwuko@gmail.com>
2020-12-15 20:19:45 +0100

    [S5] fixing heroku settings

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn server_config.wsgi --log-file -
+worker: celery -A devices.tasks worker -B --loglevel=info
+python manage.py celeryd -v 2 -B -s celery -E -l INFO
--- server_config/settings.py
+++ server_config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import datetime
 import environ
+import dj_database_url
 
 env = environ.Env()
 
@@ -100,6 +101,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
